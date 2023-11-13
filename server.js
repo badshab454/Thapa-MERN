9b151e25ba4f19f3b21f00e2b68aebe30e583b4c
Badsha Hossain <badshab454@gmail.com>
2023-11-13 21:30:21 +0600

    express js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/register", (req, res) => {
+    res.status(200).send("Welcome to Registration Page");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+})
