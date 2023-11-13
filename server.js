bd15b358ae38a972979466d3dd94380a055f6eca
Badsha Hossain <badshab454@gmail.com>
2023-11-13 22:56:22 +0600

    nodeman install

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/register", (req, res) => {
-    res.status(200).send("Welcome to Registration Page");
+  res.status(200).send("Welcome to Registernod Page");
 });
 
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running at port: ${PORT}`);
-})
+  console.log(`Server is running at port: ${PORT}`);
+});
